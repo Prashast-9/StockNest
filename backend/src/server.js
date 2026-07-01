d0d12283c636043cf86ade3a2ef0e5bd409b8442
Samaira <samaira.24cse@bmu.edu.in>
2026-07-01 18:00:18 +0530

    refactor: Remove Firebase/Google SSO, keep JWT-only authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors    = require('cors');
 require('dotenv').config();
 
-// Initialize Firebase Admin at startup
-require('./config/firebase');
+
 
 const authRoutes                        = require('./routes/authRoutes');
 const { assetsRouter, maintenanceRouter } = require('./routes/assets');
